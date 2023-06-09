90782a3c31247c3205cb163f547316383bb77189
Cydeo-Kamilla <kamillaholubieva@yahoo.com>
2023-06-08 22:03:30 -0400

    day first git

---
+++ src/test/java/com/loop/pages/LoopPracticeDragDropPage.java
@@ -0,0 +1,18 @@
+package com.loop.pages;
+
+import com.loop.test.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+public class LoopPracticeDragDropPage {
+
+    @FindBy(id="draggable")
+    public WebElement smallCircle;
+    @FindBy(id="droptarget")
+    public WebElement bigCircle;
+    public LoopPracticeDragDropPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+}
